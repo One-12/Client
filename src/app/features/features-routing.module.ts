f9bfbab76f047491c8ba9a0f1186faef6f147763
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-01-16 08:43:19 +0530

    Navigation Issue Fixed.

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -4,16 +4,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeaturesComponent } from './features.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: FeaturesComponent,
-        children: [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }],
-    },
-    { path: '**', redirectTo: '' },
+  {
+    path: '',
+    component: FeaturesComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: 'post',
+        loadChildren: () =>
+          import('./post/post.module').then(m => m.PostModule),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class FeaturesRoutingModule {}
