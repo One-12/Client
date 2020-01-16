aeebf6d74ce8d5679c5c0e068b399e75373f9c68
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-01-16 18:39:54 +0530

    UX Issues Fixed.

---
+++ src/app/features/post/components/post-details-cards/post-details-cards.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'one12-post-details-cards',
+  templateUrl: './post-details-cards.component.html',
+  styleUrls: ['./post-details-cards.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PostDetailsCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/post/post.module.ts
+++ src/app/features/post/post.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PostRoutingModule } from './post-routing.module';
 import { PostComponent } from './components/post.component';
+import { PostDetailsCardsComponent } from './components/post-details-cards/post-details-cards.component';
 
 @NgModule({
-  declarations: [PostComponent],
+  declarations: [PostComponent, PostDetailsCardsComponent],
   imports: [PostRoutingModule, CommonModule, SharedModule],
 })
 export class PostModule {}
