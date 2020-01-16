3f49b2e23270c958de2242b62754abb97f824247
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-01-16 20:58:00 +0530

    UX Issues Fixed + Code Cleaned.

--- src/app/features/post/components/post-details-cards/post-details-cards.component.ts
+++ src/app/features/post/components/post-details-cards/post-details-cards.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'one12-post-details-cards',
   templateUrl: './post-details-cards.component.html',
   styleUrls: ['./post-details-cards.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostDetailsCardsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class PostDetailsCardsComponent {}
