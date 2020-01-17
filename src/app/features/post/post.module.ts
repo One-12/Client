c3cd3dcb85a3ac24d233b014e7d568939127b302
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-01-17 11:02:51 +0530

    Comments Component Added + Formatting Issues Fixed.

--- src/app/features/post/post.module.ts
+++ src/app/features/post/post.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '../shared/shared.module';
 import { PostRoutingModule } from './post-routing.module';
 import { PostComponent } from './components/post.component';
 import { PostDetailsCardsComponent } from './components/post-details-cards/post-details-cards.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
-  declarations: [PostComponent, PostDetailsCardsComponent],
+  declarations: [PostComponent, PostDetailsCardsComponent, CommentsComponent],
   imports: [PostRoutingModule, CommonModule, SharedModule],
 })
 export class PostModule {}
