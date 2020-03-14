e1d159ef7b710653bdb70eca8374301092bdb2d3
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-03-14 21:55:01 +0530

    #1: Easter Egg Changes Merged.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'One12';
+  constructor() {
+    console.log('   \\  X  / _   ) _  _   _ _   _    _)_ _  ');
+    console.log('    \\/ \\/ )_) ( (_ (_) ) ) ) )_)   (_ (_) ');
+    console.log('         (_                 (_            ');
+
+
+    console.log(' .88888.  888888ba   88888888b    d88  d8888b. ');
+    console.log('d8\'   `8b 88    `8b  88            88      `88 ');
+    console.log('88     88 88     88 a88aaaa        88  .aaadP\' ');
+    console.log('88     88 88     88  88            88  88\'     ');
+    console.log('Y8.   .8P 88     88  88            88  88.     ');
+    console.log(' `8888P\'  dP     dP  88888888P    d88P Y88888P ');
+  }
 }
