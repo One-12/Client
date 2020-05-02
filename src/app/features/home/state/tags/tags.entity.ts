0bb0a9a22c70182035a6f14ccfe142c51fbfa64d
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-05-02 12:19:09 +0530

    Overflow Issue Fixed for Popular Posts + Merged Social Login Module 🧠

--- src/app/features/home/state/tags/tags.entity.ts
+++ src/app/features/home/state/tags/tags.entity.ts
@@ -1,16 +1,14 @@
-import * as faker from 'faker';
-
 import { TagModel } from '../../models/tag/tag.model';
 
 export class TagsEntity {
   public static getTrendingTags(): TagModel[] {
-      let tags: TagModel[] = [];
+    let tags: TagModel[] = [];
 
-      for (let i = 0; i < 10; i++) {
-          const tag = { name: faker.lorem.word(), id: faker.lorem.slug(8)};
-          tags = [...tags, tag];
-      }
+    for (let i = 0; i < 10; i++) {
+      const tag = { name: 'Hello', id: 'Hello' };
+      tags = [...tags, tag];
+    }
 
-      return tags;
+    return tags;
   }
 }
