62738be6fbb894eb0943a58eb3ff30603c8f0304
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-05-15 07:21:06 +0530

    Search Templates Page UI/UX Changes Merged

--- src/app/features/post/components/add-post/search-templates/search-templates.component.ts
+++ src/app/features/post/components/add-post/search-templates/search-templates.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EMPTY_STRING } from '../../../../shared/constants/constants';
+import { SearchTemplatesResponseModel } from '../../../models/template/search-templates-response.model';
 
 @Component({
   selector: 'search-templates',
@@ -9,11 +10,21 @@ import { EMPTY_STRING } from '../../../../shared/constants/constants';
 export class SearchTemplatesComponent implements OnInit {
   public searchQuery: string;
 
+  public searchResults: SearchTemplatesResponseModel[];
+
   public ngOnInit(): void {
     this._initializeProperties();
   }
 
   private _initializeProperties(): void {
     this.searchQuery = EMPTY_STRING;
+    this.searchResults = [];
+
+    for (let i = 0; i < 25; i++) {
+      this.searchResults.push({
+        title: `Images ${i}`,
+        imageUrl: 'https://picsum.photos/200/300',
+      });
+    }
   }
 }
