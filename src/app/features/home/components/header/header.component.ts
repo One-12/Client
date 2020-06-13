5aafdeb7b843ad523fc8c4a7ac876aed95e93e71
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-06-13 19:44:38 +0530

    Merged Documentation for Header Component.

--- src/app/features/home/components/header/header.component.ts
+++ src/app/features/home/components/header/header.component.ts
@@ -13,12 +13,21 @@ import { AuthService } from '../../../../core/auth/services/auth.service';
 export class HeaderComponent implements OnInit {
   public user: User;
 
+  /**
+   * Creates an instance of HeaderComponent.
+   * @param _router
+   * @param _authService
+   * @param _angularFireAuth
+   */
   constructor(
     private readonly _router: Router,
     private readonly _authService: AuthService,
     private readonly _angularFireAuth: AngularFireAuth,
   ) {}
 
+  /**
+   * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+   */
   public ngOnInit(): void {
     this._angularFireAuth.authState.subscribe(user => {
       this.user = user;
@@ -29,10 +38,16 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  /**
+   * Event Handler when Login Button is Clicked.
+   */
   public async onLoginButtonClicked(): Promise<void> {
     await this._router.navigate(['/login']);
   }
 
+  /**
+   * Event Handler when Logout Button is Clicked.
+   */
   public async onLogOutButtonClicked(): Promise<void> {
     await this._angularFireAuth.signOut();
   }
