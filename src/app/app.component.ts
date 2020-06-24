aeb298dee071c84a6a2dd6594e45211d8ee56ece
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-06-24 16:08:26 +0530

    Merged Background Image/ Style Changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,17 @@ export class AppComponent {
 
   public canShowScrollToTopButton: boolean;
 
-  constructor() {
+  constructor(@Inject(DOCUMENT) private readonly _document: Document) {
     console.log('   \\  X  / _   ) _  _   _ _   _    _)_ _  ');
     console.log('    \\/ \\/ )_) ( (_ (_) ) ) ) )_)   (_ (_) ');
     console.log('         (_                 (_            ');
 
     console.log(' .88888.  888888ba   88888888b    d88  d8888b. ');
-    console.log('d8\'   `8b 88    `8b  88            88      `88 ');
-    console.log('88     88 88     88 a88aaaa        88  .aaadP\' ');
-    console.log('88     88 88     88  88            88  88\'     ');
+    console.log("d8'   `8b 88    `8b  88            88      `88 ");
+    console.log("88     88 88     88 a88aaaa        88  .aaadP' ");
+    console.log("88     88 88     88  88            88  88'     ");
     console.log('Y8.   .8P 88     88  88            88  88.     ');
-    console.log(' `8888P\'  dP     dP  88888888P    d88P Y88888P ');
+    console.log(" `8888P'  dP     dP  88888888P    d88P Y88888P ");
   }
 
   @HostListener('window:scroll')
