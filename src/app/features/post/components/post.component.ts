3456291eee66a7a087f338a0cc88af8e70fb14b1
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-06-25 15:40:16 +0530

    Style Changes for Post Details Components.

--- src/app/features/post/components/post.component.ts
+++ src/app/features/post/components/post.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
@@ -8,7 +9,19 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 })
 export class PostComponent {
   public tags: string[];
-  constructor() {
-    this.tags = ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5', 'Tag 6', 'Tag 7', 'Tag 8'];
+
+  /**
+   * Creates a new instance of PostComponent
+   * @param _location
+   */
+  constructor(private readonly _location: Location) {
+    this.tags = ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'dTag 5', 'Tag 6', 'Tag 7', 'Tag 8'];
+  }
+
+  /**
+   * Navigate to Browser's Back Location when Close Button is Clicked.
+   */
+  public async onCloseButtonClicked(): Promise<void> {
+    this._location.back();
   }
 }
