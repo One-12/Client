19ca7f27877f4d0defa9214dddf8dbc7dae5a8f2
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-07-11 21:14:22 +0530

    Masonary Layout Merged for Template Selection

--- src/app/features/creators-corner/creators-corner.module.ts
+++ src/app/features/creators-corner/creators-corner.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CreatorsCornerRoutingModule } from './creators-corner-routing.module';
 import { PostTemplateListComponent } from './post-template-list/post-template-list.component';
+import { NgxMasonryModule } from 'ngx-masonry';
 
 @NgModule({
   declarations: [PostTemplateListComponent],
-  imports: [CommonModule, CreatorsCornerRoutingModule],
+  imports: [CommonModule, CreatorsCornerRoutingModule, NgxMasonryModule],
 })
 export class CreatorsCornerModule {}
