22c31d95810d4d7bc731e5fff6ab7cbecda72d1d
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-07-13 21:16:17 +0530

    Styled Creators Corner

--- src/app/features/creators-corner/creators-corner.module.ts
+++ src/app/features/creators-corner/creators-corner.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgxMasonryModule } from 'ngx-masonry';
+import { FeatherModule } from 'angular-feather';
+import { CheckCircle } from 'angular-feather/icons';
 
 import { CreatorsCornerRoutingModule } from './creators-corner-routing.module';
 import { CreatorsCornerComponent } from './components/creators-corner.component';
 import { PostTemplateListComponent } from './components/post-template-list/post-template-list.component';
 import { PostTemplateSearchComponent } from './components/post-template-search/post-template-search.component';
 
+const icons = { CheckCircle };
+
 @NgModule({
   declarations: [PostTemplateListComponent, PostTemplateSearchComponent, CreatorsCornerComponent],
-  imports: [CommonModule, CreatorsCornerRoutingModule, NgxMasonryModule],
+  imports: [CommonModule, CreatorsCornerRoutingModule, NgxMasonryModule, FeatherModule.pick(icons)],
 })
 export class CreatorsCornerModule {}
