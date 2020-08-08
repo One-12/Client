2b71a13d68e7a010155841af3187c75b6334f31e
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-08-08 15:35:34 +0530

    Studio Component Bootstrapped with Schematics.

--- src/app/features/creators-corner/creators-corner.module.ts
+++ src/app/features/creators-corner/creators-corner.module.ts
@@ -3,19 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { NgxMasonryModule } from 'ngx-masonry';
 import { FeatherModule } from 'angular-feather';
-import { CheckCircle } from 'angular-feather/icons';
+import { Box, CheckCircle, Type } from 'angular-feather/icons';
 
+import { HeaderComponent } from './components/header/header.component';
 import { StudioComponent } from './components/studio/studio.component';
-import { CreatorsCornerRoutingModule } from './creators-corner-routing.module';
 import { CreatorsCornerComponent } from './components/creators-corner.component';
 import { PostTemplateListComponent } from './components/post-template-list/post-template-list.component';
 import { PostTemplateSearchComponent } from './components/post-template-search/post-template-search.component';
-import { HeaderComponent } from './components/header/header.component';
 
-const icons = { CheckCircle };
+import { CreatorsCornerRoutingModule } from './creators-corner-routing.module';
+
+const icons = { CheckCircle, Type, Box };
 
 @NgModule({
-  declarations: [PostTemplateListComponent, PostTemplateSearchComponent, CreatorsCornerComponent, StudioComponent, HeaderComponent],
+  declarations: [
+    PostTemplateListComponent,
+    PostTemplateSearchComponent,
+    CreatorsCornerComponent,
+    StudioComponent,
+    HeaderComponent,
+  ],
   imports: [CommonModule, CreatorsCornerRoutingModule, NgxMasonryModule, FeatherModule.pick(icons)],
 })
 export class CreatorsCornerModule {}
